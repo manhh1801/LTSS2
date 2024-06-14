bc3dd8b413ecfbd990fe5e7b2244864ad08382e7
manhnguyen1801 <manhforbusiness@gmail.com>
2024-06-14 16:54:20 +0700

    working

---
+++ src/search.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../lib/mpi.h"
+
+/* Process information */
+int ProcessID, Processes;
+/*  */
+
+/* Entry procedure */
+int main(int argc, char* argv[]) {
+  /* Initializing parallel environment */
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &ProcessID);
+  MPI_Comm_size(MPI_COMM_WORLD, &Processes);
+
+  /* Shared memory */
+  int Size = 10;
+  int Array[Size] = {2,4,6,8,10,12,14,16,18,20};
+  int Value = 9;
+
+  /* Calculating */
+  int Result = 0;
+  if(Array[ProcessID] <= Value && Value < Array[ProcessID]) { Result = ProcessID; }
+  MPI_Allreduce(&Result, &Result, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+  /* Printing out result */
+  if(ProcessID == 0) {
+    printf("\nResult = %d\n", Result);
+  }
+
+  /* Terminating parallel environment */
+  MPI_Finalize();
+
+  /* Procedure exit */
+  return 0;
+}
+/*  */
